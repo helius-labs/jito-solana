aecd368d925db8421ff5e6dcccc20fccdd49481e
kyle <kyle@helius.xyz>
2025-07-28 18:12:06 -0500

    Add --public-tvu-address arg to overrider UDP shred receive address.

--- validator/src/commands/run/args.rs
+++ validator/src/commands/run/args.rs
@@ -356,6 +356,18 @@ pub fn add_args<'a>(app: App<'a, 'a>, default_args: &'a DefaultArgs) -> App<'a, 
             ),
     )
     .arg(
+        Arg::with_name("public_tvu_addr")
+            .long("public-tvu-address")
+            .alias("tvu-host-addr")
+            .value_name("HOST:PORT")
+            .takes_value(true)
+            .validator(solana_net_utils::is_host_port)
+            .help(
+                "Specify TVU address to advertise in gossip for receiving UDP shreds \
+                [default: ask --entrypoint or localhost when --entrypoint is not provided]",
+            ),
+    )
+    .arg(
         Arg::with_name("public_tpu_addr")
             .long("public-tpu-address")
             .alias("tpu-host-addr")
